0cde999c04c0daddfdfa9b8953214dd7ecd51dc7
Ryan L <hi-im-ryguy@users.noreply.github.com>
2019-02-04 20:09:13 -0800

    Update 0001_Two_Sum.cs

--- C#/0001_Two_Sum.cs
+++ C#/0001_Two_Sum.cs
@@ -1,15 +1,12 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-    for(var i = 0; i < nums.length; i++){
-        for(var j = i + 1; j < nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return [i, j]
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.Length; i++) {
+            for(int j = i + 1; j < nums.Length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
             }
         }
+        return new int[] {};
     }
-    return []
-};
+}
