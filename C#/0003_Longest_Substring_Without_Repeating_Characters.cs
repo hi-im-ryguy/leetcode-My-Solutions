cadb10bb4c4069526d4fdc8d087654a101e28468
Ryan L <hi-im-ryguy@users.noreply.github.com>
2019-04-15 16:58:56 -0700

    Update 0003_Longest_Substring_Without_Repeating_Characters.cs

--- C#/0003_Longest_Substring_Without_Repeating_Characters.cs
+++ C#/0003_Longest_Substring_Without_Repeating_Characters.cs
@@ -1,22 +1,34 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s)
         {
-            int longestLength = 0;
-            Queue<char> uniqueCharacters = new Queue<char>();
-            foreach (char letter in s)
+        // Because our only goal is the return the largest length of a non-repeating substring, regardless
+        // of length, we can initialize a field to keep track of that at 0
+        int longestLength = 0;
+
+        // I decided to use a Queue, because I want to be able to easily dequeue a string's characters,
+        // until it reaches the duplicate characters, because we want a SUBSTRING not a SUBSEQUENCE
+        Queue<char> uniqueCharacters = new Queue<char>();
+        
+        // Let's iterate through the string s. Because C# treats strings as an array of char, we can do this easily with foreach
+        foreach (char letter in s)
+        {
+            
+            // Check to see if this letter is repeated, if it is, remove all characters BEFORE it
+            // until it reaches the duplicate letter, including the duplicate letter itself
+            while (uniqueCharacters.Contains(letter))
             {
-                while (uniqueCharacters.Contains(letter))
-                {
-                    uniqueCharacters.Dequeue();
-                }
-
-                uniqueCharacters.Enqueue(letter);
-
-                if (uniqueCharacters.Count > longestLength)
-                {
-                    longestLength++;
-                }
+                uniqueCharacters.Dequeue();
             }
-            return longestLength;
+            
+            // No matter what happens, queue this character into the Queue.
+            uniqueCharacters.Enqueue(letter);
+            
+            // Increment longestLength whenever the substring is bigger than the "last" biggest substring
+            if (uniqueCharacters.Count > longestLength)
+            {
+                longestLength++;
+            }
         }
+        return longestLength;
+    }
 }
